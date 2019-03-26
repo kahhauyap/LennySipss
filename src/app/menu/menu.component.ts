7657f530690054d56b96a2262a3ae515c5206947
Kah Hau Yap <kahhauyap@gmail.com>
2019-03-25 22:25:36 -0400

    Implemented async data fetching

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Drinks } from '../menu';
+import { DrinkService } from '../drink.service';
+import { Drink } from '../drink';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent implements OnInit {
 
-  drinks = Drinks;
+  drinks: Drink[];
 
-  constructor() { }
+  constructor(private drinkService: DrinkService) { }
 
   ngOnInit() {
+    this.getDrinks();
+  }
+
+  getDrinks(): void {
+    this.drinkService.getDrinks()
+      .subscribe(drinks => this.drinks = drinks);
   }
 
   onClick(): void {
     alert("yo!");
   }
-
 }
