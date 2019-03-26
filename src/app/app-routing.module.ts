ab0a0137de6a60ce67fde4f976d8462d9583ab9e
Kah Hau Yap <kahhauyap@gmail.com>
2019-03-26 14:14:33 -0400

    Deployed app and added Modal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { BlogComponent } from './blog/blog.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
   { path: 'bar', component: BarComponent },
   { path: 'blog', component: BlogComponent },
 ];
